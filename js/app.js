04b9c57d1e4af61017cd600cdbc8f98b0f9f6dfe
Shamim742 <shamimislam2742@gmail.com>
2023-02-26 13:23:48 +0600

    show n hide spinner

--- js/app.js
+++ js/app.js
@@ -32,15 +32,29 @@ const displayPhones = phones => {
   </div>
   `;
         phonesContainer.appendChild(phoneDiv)
-    })
+    });
+    // stop loader
+    toggleSpinner(false);
 };
 
-
+// handle search button clicked
 document.getElementById('btn-search').addEventListener('click',function(){
+    // start loader
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     document.getElementById('search-field').value = '';
     loadPhones(searchText)
-})
+});
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+};
 
 // loadPhones();
