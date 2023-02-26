d0e5b0b6f9661cf0b10619987d6254244d8a5a54
Shamim742 <shamimislam2742@gmail.com>
2023-02-27 00:41:00 +0600

    show phone details on modal

--- js/app.js
+++ js/app.js
@@ -10,19 +10,19 @@ const displayPhones = (phones, dataLimit) => {
     phonesContainer.innerText = '';
     // display 10 phones only
     const showAll = document.getElementById('show-all');
-    if(dataLimit && phones.length > 10){
+    if (dataLimit && phones.length > 10) {
         phones = phones.slice(0, 10);
         showAll.classList.remove('d-none');
     }
-    else{
+    else {
         showAll.classList.add('d-none')
     }
     // display no phones
     const noPhone = document.getElementById('no-found-message')
-    if(phones.length === 0){
+    if (phones.length === 0) {
         noPhone.classList.remove('d-none');
     }
-    else{
+    else {
         noPhone.classList.add('d-none')
     }
     // display all phones
@@ -35,7 +35,7 @@ const displayPhones = (phones, dataLimit) => {
     <div class="card-body">
       <h5 class="card-title">${phone.phone_name}</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+      <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
     </div>
   </div>
   `;
@@ -55,36 +55,49 @@ const processSearch = (dataLimit) => {
 };
 
 // handle search button clicked
-document.getElementById('btn-search').addEventListener('click',function(){
+document.getElementById('btn-search').addEventListener('click', function () {
     processSearch(10);
 });
 
 // search input field enter key handler
-document.getElementById('search-field').addEventListener('keypress',function(event){
-    if(event.key == 'Enter'){
+document.getElementById('search-field').addEventListener('keypress', function (event) {
+    if (event.key == 'Enter') {
         processSearch(10);
     };
 });
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(isLoading){
+    if (isLoading) {
         loaderSection.classList.remove('d-none')
     }
-    else{
+    else {
         loaderSection.classList.add('d-none')
     }
 };
 // not the best way to load show all
-document.getElementById('btn-show-all').addEventListener('click',function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 });
 
-const loadPhoneDetails = async id =>{
+const loadPhoneDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayPhoneDetails(data.data);
 };
 
-// loadPhones();  
+const displayPhoneDetails = phone => {
+    console.log(phone)
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone_details');
+    phoneDetails.innerHTML = `
+    <img class="img-fluid" src="${phone.image}" alt="">
+    <p>Release Date:${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+    <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Found'}</p>
+    <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Found'}</p>
+    `
+};
+
+loadPhones('apple');  
